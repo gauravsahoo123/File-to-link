c174658e933330968ac1023e0e8bdb0f071ad97f
gauravsahoo123 <158731701+gauravsahoo123@users.noreply.github.com>
2024-02-16 20:42:03 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -17,7 +17,7 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '50'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002040564082'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '212.224.88.31'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "52014953230").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
